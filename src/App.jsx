d4d0be2ded7210b7b74ea461ee063d8943967b49
long66339-ux <long66339@gmail.com>
2025-12-16 00:07:50 +0800

    remove api key

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,8 @@ const firebaseConfig = isGeminiEnv && typeof __firebase_config !== 'undefined'
     appId: "1:575049200842:web:693c26a0b1d2f1b7357033",
     measurementId: "G-WJJ70DQ9RX" }; 
 
-const apiKey = "AIzaSyDQZzNOlmdXyg-mVSBr2Bu3yOP1tc-Yvdk"; // API Key (在此環境由系統提供，若獨立部署請填入您的 Key)
+// ✅ 正確寫法 (Vite)
+const apiKey = import.meta.env.VITE_API_KEY; // API Key (在此環境由系統提供，若獨立部署請填入您的 Key)
 
 // --------------------------------------------------------------------------------
 // Helper Functions
